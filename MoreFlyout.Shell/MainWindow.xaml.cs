f7d647140eaa52e01a7a8ddc2722fed363a87daa
ChenYiLins <1062790901@qq.com>
2024-07-24 12:46:01 +0800

    Feature: MainWindow

---
+++ MoreFlyout.Shell/MainWindow.xaml.cs
@@ -0,0 +1,41 @@
+﻿using System.Diagnostics;
+using System.Runtime.InteropServices;
+using Microsoft.UI.Xaml;
+using MoreFlyout.Shell.Helpers;
+
+using Windows.UI.ViewManagement;
+
+namespace MoreFlyout.Shell;
+
+public sealed partial class MainWindow : WindowEx
+{
+    private Microsoft.UI.Dispatching.DispatcherQueue dispatcherQueue;
+
+    private UISettings settings;
+
+    public MainWindow()
+    {
+        InitializeComponent();
+
+        AppWindow.SetIcon(Path.Combine(AppContext.BaseDirectory, "Assets/WindowIcon.ico"));
+        Content = null;
+        Title = "AppDisplayName".GetLocalized();
+        ExtendsContentIntoTitleBar = true;
+
+        // Theme change code picked from https://github.com/microsoft/WinUI-Gallery/pull/1239
+        dispatcherQueue = Microsoft.UI.Dispatching.DispatcherQueue.GetForCurrentThread();
+        settings = new UISettings();
+        settings.ColorValuesChanged += Settings_ColorValuesChanged; // cannot use FrameworkElement.ActualThemeChanged event
+    }
+
+    // this handles updating the caption button colors correctly when windows system theme is changed
+    // while the app is open
+    private void Settings_ColorValuesChanged(UISettings sender, object args)
+    {
+        // This calls comes off-thread, hence we will need to dispatch it to current app's thread
+        dispatcherQueue.TryEnqueue(() =>
+        {
+            TitleBarHelper.ApplySystemThemeToCaptionButtons();
+        });
+    }
+}
